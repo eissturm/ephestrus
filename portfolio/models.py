1d14aacf56ce81b31a03fbc81356f76aa5926033
Andy <andrew@ephestr.us>
2011-11-11 00:38:16 -0600

    Removed unneeded files, began work on 'portfolio' app

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from markupfield.fields import MarkupField
 
 # Create your models here.
+class PortItem(models.Model):
+    name = models.CharField(max_length=50)
+    url = models.URLField()
+    description = MarkupField(default_markup_type='markdown')
+    img = models.ImageField(upload_to='portfolio')
+
+class Site(PortItem):
+    client = models.CharField(max_length=150)
+    services = models.CharField(max_length=75)
+
+class App(PortItem):
+    pass
